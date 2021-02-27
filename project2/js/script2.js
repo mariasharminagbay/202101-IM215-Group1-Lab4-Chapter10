2bd3489e2ad36678cc929408cb1367571b0a15d8
Vaidehi <Vdarji@confederationcollege.ca>
2021-02-26 22:01:09 -0500

    Project 2 by sharmin & vaidehi

--- project2/js/script2.js
+++ project2/js/script2.js
@@ -1,34 +1,36 @@
-  
-$(document).ready(function() {
+$(function () {
+
     //populate with the images
     images.forEach(element => {
         var image = $("<img></img>").attr('src', 'images/square/' + element.path).attr('alt', element.title);
         $(".gallery").append(image);
     });
 
+
     //mouseenter
-    $("img").mouseenter(function() {
+    $("img").mouseenter(function () {
+
         $(this).addClass("gray");
         var divprvw = $("<div></div>").attr('id', 'preview');
-        var img1 = $("<img></img>").attr('src', $(this).attr('src').replace('square', 'medium')).attr('alt', $(this).attr('alt'));
-        divprvw.append(img1);
+        var image = $("<img></img>").attr('src', $(this).attr('src').replace('square', 'medium')).attr('alt', $(this).attr('alt'));
+        divprvw.append(image);
         images.forEach(element => {
             if (element.title == $(this).attr('alt')) {
-                var p = $("<p></p>").html(element.title + " " + element.city + ", " + element.country + " [" + element.taken + "]");
+                var p = $("<p></p>").html(element.title + " <br>" + element.city + ", " + element.country + " [" + element.taken + "]");
                 divprvw.append(p);
             }
         });
-        $("body").append(divprvw);
-    });
-    //mouseleave
-    $("img").mouseleave(function() {
-        $("#preview").remove();
-        $(this).removeClass("gray");
-    });
 
-    //mousemove
-    $("img").mousemove(function(event) {
-        $("#preview").css('left', event.pageX).css('top', event.pageY);
+        //step 6
+        $("#preview").css('left', event.pageX + 10).css('top', event.pageY + 10);
+        // step 7
+        $("body").append(divprvw);
+
+    }).on('mouseleave', function (e) {    //mouse leave
+        $('#preview').remove();
+        $(this).removeClass('gray');
+    }).on('mousemove', function (e) {         //mouse move
+        $('#preview').css("display", "block").css("top", (e.pageY - 10) + "px").css("left", (e.pageX + 30) + "px");
     });
 
 });
